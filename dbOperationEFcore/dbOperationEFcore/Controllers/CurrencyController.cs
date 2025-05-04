f6cb0f47607e6666ca4467d49a5ae96bf0e6db37
Rushikesh Gaikwad <rushikeshhtc@gmail.com>
2025-05-04 09:25:06 +0530

    enetity  done

--- dbOperationEFcore/dbOperationEFcore/Controllers/CurrencyController.cs
+++ dbOperationEFcore/dbOperationEFcore/Controllers/CurrencyController.cs
@@ -1,6 +1,8 @@
 ﻿using dbOperationEFcore.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace dbOperationEFcore.Controllers
 {
@@ -20,6 +22,16 @@ namespace dbOperationEFcore.Controllers
         {
             //var result = _dbContext.currencies.ToList();
             var result = (from currencies in _dbContext.currencies select currencies).ToList();
+            return Ok(result);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> getCurrency()
+        {
+            //var result = _dbContext.currencies.ToList();
+            var result = await _dbContext.currencies.FromSqlRaw("select * from cuurency").ToListAsync();
+            //var result1 = await _dbContext.currencies.ExecuteUpdateAsync("update cuurency set title=holly where id=2");
             return Ok(result);
         }
 
